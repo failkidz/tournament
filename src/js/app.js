1738c5ef551704f052ef2f3f3053f1d3456ebcde
Carl Eriksson <carle293@hotmail.com>
2015-02-25 01:29:05 +0100

    First working react render

---
+++ src/js/app.js
@@ -0,0 +1,7 @@
+var App = require('./components/App.react');
+var React = require('react');
+
+React.renderComponent(
+	<App />,
+	document.getElementById('react')
+);
---
+++ src/js/components/App.react.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				<p>I am satisfield</p>
+			</div>
+		);
+	}
+});
+
+module.exports = App;
